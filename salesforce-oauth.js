ade81fabd5b9041c0cf4d83ae8fa573d403e390a
devriesewouter89 <devriese.wouter@gmail.com>
2017-08-29 14:58:27 +0200

    adapted code to Masterlist

--- salesforce-oauth.js
+++ salesforce-oauth.js
@@ -1,6 +1,5 @@
 const express = require('express'),
     oauth2 = require('salesforce-oauth2'),
-    // cookie = require('node-cookie'),
     axios = require('axios'),
     chalk = require('chalk');
 
@@ -10,21 +9,16 @@ const callbackUrl = "http://localhost:3000/oauth/callback",
     consumerKey = "3MVG9sSN_PMn8tjRbbCIah13NOUQh0FuVxv7you0LJt3TuRf9orUc9Z.A9YFVW9j_Of2kx2TaGwqWn0VUWTOg",
     consumerSecret = "2998375974546207857";
 
-// const urlResource = 'https://iminds--test.cs89.my.salesforce.com/services/data/v20.0/';
-// const urlObjects = 'https://iminds--test.cs89.my.salesforce.com/services/data/v20.0/sobjects';
-// const urlReports =  ' https://iminds.my.salesforce.com/services/data/v20.0/query?q=SELECT+name+from+Account';
 const urlResource = 'https://iminds.my.salesforce.com/services/data/v20.0/';
 const urlObjects = 'https://iminds.my.salesforce.com/services/data/v20.0/sobjects/';
 const urlReports = 'https://iminds.my.salesforce.com/services/data/v20.0/query?q=SELECT+name+from+Report';
-const csvQuery = 'https://iminds.my.salesforce.com/services/data/v20.0/query?q=SELECT+name+from+Report';
-// Record Type equals iStart project
-// AND Call: Call Name equals iStart-2017-1
-// const urlObjects =  'https://iminds--test.cs89.my.salesforce.com/services/data/v20.0/sobjects';
+const urlPreselection = 'https://iminds.my.salesforce.com/services/data/v35.0/analytics/reports/00Ob0000004JUf4?includeDetails=true';
+const urlSelection = 'https://iminds.my.salesforce.com/services/data/v35.0/analytics/reports/00Ob0000004JUud?includeDetails=true';
+
 
 const app = express();
 axios.create({
     baseURL: 'https://iminds.my.salesforce.com',
-    // baseURL: 'https://iminds--test.cs89.my.salesforce.com',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
@@ -36,7 +30,6 @@ let resources, objects, istart, reports;
 app.get("/", function (request, response) {
     // console.log('> /');
     const uri = oauth2.getAuthorizationUrl({
-        // base_url: 'https://iminds--test.cs89.my.salesforce.com',
         base_url: 'https://iminds.my.salesforce.com',
         redirect_uri: callbackUrl,
         client_id: consumerKey,
@@ -50,10 +43,9 @@ async function callAll() {
     "use strict";
     console.log(chalk.bgRed('>> callAll'));
     const [resources, objects, istart, accounts] = await Promise.all([
-        // list(urlResource, options),
         list(urlResource, options),
         list(urlObjects, options),
-        list(csvQuery, options),
+        list(urlPreselection, options),
         list(urlReports, options)]);
 
     // console.log(chalk.bgRed('response?'), JSON.stringify(istart, null, '  '));
@@ -67,7 +59,6 @@ app.get('/oauth/callback', function (request, response) {
     let authorizationCode = request.param('code');
 
     oauth2.authenticate({
-        // base_url: 'https://iminds--test.cs89.my.salesforce.com',
         base_url: 'https://iminds.my.salesforce.com',
         redirect_uri: callbackUrl,
         client_id: consumerKey,
@@ -100,69 +91,110 @@ app.get('/oauth/callback', function (request, response) {
                     'X-PrettyPrint': '1'
                 }
             };
-            [resources, objects, istart, reports] = callAll();
-            // callAll();
-                // .then(result =>{
-                //     console.log(chalk.bgRed('result callAll'), JSON.stringify(resources, null, '  '));
-                //     response.send(`<html><body><p>Resources:<pre >${JSON.stringify(resources, null, '  ')}</pre>  </p><p>Accounts:<pre> ${JSON.stringify(objects, null, '  ')}</pre>  </p></body></html>`)})
-                // .catch(err => console.log(err));
 
-            // callAllPromise().then(result => {
-            //     console.log(chalk.bgGreen('to front-end'));
-            //     // response.send(`<html><body><p>Resources:<pre > ${JSON.stringify(resources, null, '  ')}</pre>  </p><p>Query:<pre> ${JSON.stringify(reports, null, '  ')}</pre>  </p></body></html>`);//
-            //
-            //     let myJSON = JSON.stringify(reports, null, '  ');
-            //     let obj = JSON.parse(myJSON);
-            //     let keys = Object.keys(obj);
-            //     // for (let i = 0; i < keys.length; i++) {
-            //     //     // console.log("[",i,"] ",keys[i],": ", obj[keys[i]]);
-            //     // }
-            //     console.log(obj[keys[2]]);
-            //     let innerJSON = JSON.stringify(obj[keys[2]], null, ' ');
-            //     // console.log(innerJSON);
-            //     let innerObj = JSON.parse(innerJSON);
-            //     let innerKeys = Object.keys(innerObj);
-            //     for (let i = 0; i < innerKeys.length; i++) {
-            //         let inner2JSON = JSON.stringify(innerObj[innerKeys[i]], null, ' ');
-            //         // console.log("[",i,']',innerJSON, inner2JSON);
-            //         let inner2Obj = JSON.parse(inner2JSON);
-            //         if (inner2Obj.hasOwnProperty('Name')) {
-            //             if (inner2Obj['Name'] === wantedReport) {
-            //                 // console.log('[', i, ']', ' name: ' + inner2Obj['Name']);
-            //                 // console.log("[", i, "] ", innerObj[innerKeys[i]]);//, ": ", innerKeys[i]
-            //                 let wantedUrl = 'https://iminds.my.salesforce.com' + innerObj[innerKeys[i]]['attributes']['url'];
-            //                 console.log(wantedUrl);
-            //                 let wantedJson;
-            //                 list(wantedUrl, options).then(result => {
-            //                     wantedJson = result;
-            //                     response.send(`<html><body><p>file:<pre > ${JSON.stringify(result, null, '  ')}</pre>  </p></body></html>`);
-            //
-            //                 });
-            //             }
-            //         } //else console.log('- geen token???:');
-            //         // let inner2Keys = Object.keys(inner2Obj);
-            //     }
-            // }).catch(err => console.log(err))
+            /* error in node/wouterDV-combo logic??*/
+            // [resources, objects, istart, reports] = callAll();
+            // // callAll();
+            //     // .then(result =>{
+            //     //     console.log(chalk.bgRed('result callAll'), JSON.stringify(resources, null, '  '));
+            //     //     response.send(`<html><body><p>Resources:<pre >${JSON.stringify(resources, null, '  ')}</pre>  </p><p>Accounts:<pre> ${JSON.stringify(objects, null, '  ')}</pre>  </p></body></html>`)})
+            //     // .catch(err => console.log(err));
+            /* to retrieve link to wanted report if not given:
+            callAllPromise().then(result => {
+                console.log(chalk.bgGreen('to front-end'));
+                // response.send(`<html><body><p>Resources:<pre > ${JSON.stringify(resources, null, '  ')}</pre>  </p><p>Query:<pre> ${JSON.stringify(reports, null, '  ')}</pre>  </p></body></html>`);//
+
+                let myJSON = JSON.stringify(reports, null, '  ');
+                let obj = JSON.parse(myJSON);
+                let keys = Object.keys(obj);
+                // for (let i = 0; i < keys.length; i++) {
+                //     console.log("[",i,"] ",keys[i],": ", obj[keys[i]]);
+                // }
+                // console.log(obj[keys[2]]);
+                let innerJSON = JSON.stringify(obj[keys[2]], null, ' ');
+                // console.log(innerJSON);
+                let innerObj = JSON.parse(innerJSON);
+                let innerKeys = Object.keys(innerObj);
+                for (let i = 0; i < innerKeys.length; i++) {
+                    let inner2JSON = JSON.stringify(innerObj[innerKeys[i]], null, ' ');
+                    // console.log("[",i,']',innerJSON, inner2JSON);
+                    let inner2Obj = JSON.parse(inner2JSON);
+                    if (inner2Obj.hasOwnProperty('Name')) {
+                        if (inner2Obj['Name'] === wantedReport) {
+                            // console.log('[', i, ']', ' name: ' + inner2Obj['Name']);
+                            // console.log("[", i, "] ", innerObj[innerKeys[i]]);//, ": ", innerKeys[i]
+                            let wantedUrl = 'https://iminds.my.salesforce.com' + innerObj[innerKeys[i]]['attributes']['url'];
+                            console.log(wantedUrl);
+                            let wantedJson;
+                            list(wantedUrl, options).then(result => {
+                                wantedJson = result;
+                                response.send(`<html><body><p>file:<pre > ${JSON.stringify(result, null, '  ')}</pre>  </p></body></html>`);
+                            });
+                        }
+                    } //else console.log('- geen token???:');
+                    // let inner2Keys = Object.keys(inner2Obj);
+                }
+            }).catch(err => console.log(err));
+            */
+            callAllPromise().then(result => {
+                console.log(chalk.bgGreen('to front-end'));
+                let myJSON = JSON.stringify(istart, null, '  ');
+                let obj = JSON.parse(myJSON);
+                let keys = Object.keys(obj);
+                // console.log('- keys.length:', keys.length);
+                for (let i = 0; i < keys.length; i++) {
+                    // console.log("[",i,"] ",keys[i],": ", obj[keys[i]]);
+                    // }
+                    // console.log(obj[keys[2]]);
+                    let innerJSON = JSON.stringify(obj[keys[i]], null, ' ');
+                    // console.log(innerJSON);
+                    let innerObj = JSON.parse(innerJSON);
+                    let innerKeys = Object.keys(innerObj);
+                    // console.log('[', i, ']', '- innerKeys.length:', innerKeys.length);
+                    for (let j = 0; j < innerKeys.length; j++) {//innerKeys.length
+                        //inner2JSON is the JSON containing JSON which needs to be unpacked 1 level lower to have all rows in new objects
+                        let inner2JSON = JSON.stringify(innerObj[innerKeys[j]], null, ' ');
+                        // console.log("[",i,']',innerJSON, inner2JSON);
+                        // console.log('- inner2JSON:', inner2JSON);
+                        let inner2Obj = JSON.parse(inner2JSON);
+                        let inner2Keys = Object.keys(inner2Obj);
+                        // console.log('[', j, ']', '- inner2Keys.length:', inner2Keys.length);
+                        for (let k = 0; k < inner2Keys.length; k++) {
+                            // console.log(chalk.bgCyan("[",i,']: '),inner2Keys[i]," object: ", inner2Obj[inner2Keys[i]]);
+                            let inner3JSON = JSON.stringify(inner2Obj[inner2Keys[k]], null, ' ');
+                            // console.log("[",i,']',chalk.red(inner2JSON), inner3JSON);
+                            // console.log('- inner3JSON:', inner3JSON);
+                            let inner3Obj = JSON.parse(inner3JSON);
+                            let inner3Keys = Object.keys(inner3Obj);
+                            // console.log('[', k, ']', '- inner3Keys.length:', inner3Keys.length);
+
+                            for (let l = 0; l < inner3Keys.length; l++) {
+                                console.log(chalk.bgCyan("[", j," ",k, ",", l, ']: '), inner3Keys[l], " object: ", inner3Obj[inner3Keys[l]]);
+                            }
+                        }
+                    }
+                }
+            }).catch(err => console.log(err))
         }
         /*
 
         The payload should contain the following fields:
 
-        id 				A URL, representing the authenticated user,
+        id              A URL, representing the authenticated user,
                         which can be used to access the Identity Service.
 
-        issued_at		The time of token issue, represented as the
+        issued_at       The time of token issue, represented as the
                         number of seconds since the Unix epoch
                         (00:00:00 UTC on 1 January 1970).
 
-        refresh_token	A long-lived token that may be used to obtain
+        refresh_token   A long-lived token that may be used to obtain
                         a fresh access token on expiry of the access
                         token in this response.
 
-        instance_url	Identifies the Salesforce instance to which API
+        instance_url    Identifies the Salesforce instance to which API
                         calls should be sent.
 
-        access_token	The short-lived access token.
+        access_token    The short-lived access token.
 
 
         The signature field will be verified automatically and can be ignored.
@@ -193,10 +225,10 @@ async function callAllPromise() {
                         .then(result => {
                             reports = result;
                             // console.log(chalk.bgCyan('reports'));
-                            return list(csvQuery, options)
+                            return list(urlPreselection, options)
                                 .then(result => {
                                     istart = result;
-                                    console.log(chalk.bgCyan('istart'), istart);
+                                    // console.log(chalk.bgCyan('istart'), istart);
                                 })
                         }).catch((err) => console.log(chalk.bgRed('error in callAllPromise'), err))
                 })
